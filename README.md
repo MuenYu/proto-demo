2e920e92948d5761bedd48f667a5770ef1c4db9f
parasomn1a <parasomnia97@gmail.com>
2022-06-28 00:42:13 +0800

    complete client & server

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Learn how to use proto
 
-- Define a proto target in a `.proto` file
-- Gen golang code from `.proto` file
-- Build a client and a server transport data via protobuf
+## Define a proto target in a `.proto` file
+// TODO
+
+## Gen golang code from `.proto` file
+1. install protoc and protoc-gen-go
+```cmd
+scoop install protobuf
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+```
+2. run the command
+```cmd
+protoc --proto_path=proto --go_out=app proto/*.proto
+```
+
+## Build a client and a server transport data via protobuf
+// TODO
+
 
 # Reference
 - [how to gen go code from proto](https://developers.google.com/protocol-buffers/docs/reference/go-generated#package)
+- [The import path must contain at least one period ('.') or forward slash ('/') character](https://github.com/techschool/pcbook-go/issues/3)
--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"google.golang.org/protobuf/proto"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"proto-demo/app/pb"
+	"time"
 )
 
 func main() {
@@ -14,9 +24,61 @@ func main() {
 }
 
 func server() {
-
+	r := gin.New()
+	r.POST("/login", func(c *gin.Context) {
+		log.Println("server", "request get!")
+		var (
+			loginReq pb.LoginReq
+		)
+		if err := c.ShouldBindWith(&loginReq, binding.ProtoBuf); err != nil {
+			c.ProtoBuf(http.StatusUnavailableForLegalReasons, &pb.LoginRes{
+				Code: 1,
+				Msg:  err.Error(),
+				Data: nil,
+			})
+		} else {
+			c.ProtoBuf(http.StatusOK, &pb.LoginRes{
+				Code: 0,
+				Msg:  fmt.Sprintf("hello %s, your password is %s", loginReq.Username, loginReq.Password),
+				Data: nil,
+			})
+		}
+	})
+	r.Run(":8080")
 }
 
 func client() {
+	var (
+		client = &http.Client{}
+		req    *http.Request
+		reqPb  = &pb.LoginReq{
+			Username: "admin",
+			Password: "jsangorno",
+		}
+		err       error
+		data      []byte
+		resp      *http.Response
+		respBytes []byte
+		respPb    = &pb.LoginRes{}
+	)
 
+	data, err = proto.Marshal(reqPb)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	req, err = http.NewRequest("POST", "http://127.0.0.1:8080/login", bytes.NewReader(data))
+	req.Header.Add("Content-Type", "application/protobuf")
+	for true {
+		time.Sleep(time.Second)
+		if resp, err = client.Do(req); err != nil {
+			log.Fatalln(err.Error())
+		}
+		if respBytes, err = ioutil.ReadAll(resp.Body); err != nil {
+			log.Fatalln(err.Error())
+		}
+		if err = proto.Unmarshal(respBytes, respPb); err != nil {
+			log.Fatalln(err.Error())
+		}
+		log.Println("client", "response get: ", respPb)
+	}
 }
